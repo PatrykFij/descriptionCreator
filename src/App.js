6c75908d28a1a36d3fd1b051e4f96cdd5e9e2659
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2021-08-27 22:09:44 +0200

    Added sass and mock file

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
-import "./App.css";
+import "./App.scss";
 import { useState } from "react";
 import { Typography, Snackbar } from "@material-ui/core";
 import { MainWrapper, StyledButton } from "./App.css.js";
 import { Form } from "./components/Form/Form";
 import { Preview } from "./components/Preview/Preview";
+import { Mock } from "./constants/Mock";
 
 const App = () => {
   const [open, setOpen] = useState(false);
-  const [producer, setProducer] = useState("KEMON");
-  const [topHeader, setTopHeader] = useState("ZESTAW DO WŁOSÓW PRZESUSZONYCH");
-  const [middleHeader, setMiddleHeader] = useState("ODŻYWIENIE SUCHYCH WŁOSÓW");
-  const [bottomHeader, setBottomHeader] = useState("Szampon 250ml, Odżywka 150ml, Krem 150ml");
+  const [producer, setProducer] = useState(Mock.producer);
+  const [topHeader, setTopHeader] = useState(Mock.topHeader);
+  const [middleHeader, setMiddleHeader] = useState(Mock.middleHeader);
+  const [bottomHeader, setBottomHeader] = useState(Mock.bottomHeader);
 
-  const [firstParagraph, setFirstParagraph] = useState(
-    "Zestaw Kemon Actyva Nutrizione to pielęgnacja przeznaczona do włosów średnich i cienkich, lekko przesuszonych, wrażliwych oraz normalnej skóry głowy. Szampon, odżywka oraz krem zapewniają optymalne i natychmiastowe odżywienie, zachowując naturalną równowagę skóry głowy. Nawilżają i chronią włosy. Dodają włosom blasku, miękkości, lekkiej objętości, ułatwiają rozczesywanie oraz trwale chronią przed przesuszeniem."
-  );
-  const [secondParagraph, setSecondParagraph] = useState(
-    "Włosy suche charakteryzują się częściową lub całkowitą utratą lipidów lub odwodnieniem. Pozbawione naturalnych substancji łuski zewnętrznej warstwy włosów otwierają się. W efekcie włosy stają się matowe, szorstkie, kruche, elektryzują się, są trudne do rozczesania i ułożenia oraz brakuje im elastyczności. Włosy mogą być z natury suche lub ulec wysuszeniu pod wpływem czynników zewnętrznych lub nawyków danej osoby. Z myślą o pielęgnacji włosów suchych Kemon Actyva proponuje linię Nutrizione z zaawansowaną technologicznie mieszanką surowców oraz składników aktywnych pochodzenia naturalnego, która wszystkim rodzajom włosów przywraca naturalny stan równowagi."
-  );
+  const [firstParagraph, setFirstParagraph] = useState(Mock.firstParagraph);
+  const [secondParagraph, setSecondParagraph] = useState(Mock.secondParagraph);
 
   const [enabledListSection, setEnabledListSection] = useState(false);
-  const [listItems, setListItems] = useState([
-    "DELIKATNIE OCZYSZCZA WŁOSY I SKÓRĘ GŁOWY",
-    "NAWILŻA I CHRONI WŁOSY",
-    "ODŻYWIA",
-    "POMAGA ZACHOWAĆ NATURALNĄ RÓWNOWAGĘ SKÓRY GŁOWY",
-    "UŁATWIA ROZCZESYWANIE",
-    "DODAJE BLASKU, MIĘKKOŚCI I LEKKOŚCI",
-  ]);
+  const [listItems, setListItems] = useState(Mock.listItems);
 
   const [enabledBannerSection, setEnabledBannerSection] = useState(false);
-  const [bannerLink, setBannerLink] = useState(
-    "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-nutrizione-kosmetyki-do-wlosow-suchych.jpg"
-  );
+  const [bannerLink, setBannerLink] = useState(Mock.bannerLink);
 
   const [enabledPicturesSection, setEnabledPicturesSection] = useState(false);
-  const [pictureSectionTitle, setPictureSectionTitle] = useState("PRZYKŁADOWY TYTUŁ SEKCJI ZE ZDJĘCIAMI");
-  const [pictureItems, setPictureItems] = useState([
-    {
-      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-disciplina.jpg",
-      title: "Kemon actyva disciplina",
-      description: "Kontrola włosów kręconych i nieposłusznych",
-    },
-    {
-      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-nuova-fibra.jpg",
-      title: "Kemon nuova fibra",
-      description: "Wzmocnienie włosów słabych i uszkodzonych",
-    },
-    {
-      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-colore-brillante.jpg",
-      title: "Kemon colore brillante",
-      description: "Wzmocnienie, ochrona i blask koloru",
-    },
-  ]);
+  const [pictureSectionTitle, setPictureSectionTitle] = useState(Mock.pictureSectionTitle);
+  const [pictureItems, setPictureItems] = useState(Mock.pictureItems);
 
   const handleCopyDescriptionCode = () => {
     var previewCode = document.getElementById("preview").innerHTML;
---
+++ src/constants/Mock.js
@@ -0,0 +1,40 @@
+export const Mock = {
+  producer: "KEMON",
+
+  topHeader: "ZESTAW DO WŁOSÓW PRZESUSZONYCH",
+  middleHeader: "ODŻYWIENIE SUCHYCH WŁOSÓW",
+  bottomHeader: "Szampon 250ml, Odżywka 150ml, Krem 150ml",
+
+  firstParagraph:
+    "Zestaw Kemon Actyva Nutrizione to pielęgnacja przeznaczona do włosów średnich i cienkich, lekko przesuszonych, wrażliwych oraz normalnej skóry głowy. Szampon, odżywka oraz krem zapewniają optymalne i natychmiastowe odżywienie, zachowując naturalną równowagę skóry głowy. Nawilżają i chronią włosy. Dodają włosom blasku, miękkości, lekkiej objętości, ułatwiają rozczesywanie oraz trwale chronią przed przesuszeniem.",
+  secondParagraph:
+    "Włosy suche charakteryzują się częściową lub całkowitą utratą lipidów lub odwodnieniem. Pozbawione naturalnych substancji łuski zewnętrznej warstwy włosów otwierają się. W efekcie włosy stają się matowe, szorstkie, kruche, elektryzują się, są trudne do rozczesania i ułożenia oraz brakuje im elastyczności. Włosy mogą być z natury suche lub ulec wysuszeniu pod wpływem czynników zewnętrznych lub nawyków danej osoby. Z myślą o pielęgnacji włosów suchych Kemon Actyva proponuje linię Nutrizione z zaawansowaną technologicznie mieszanką surowców oraz składników aktywnych pochodzenia naturalnego, która wszystkim rodzajom włosów przywraca naturalny stan równowagi.",
+  listItems: [
+    "DELIKATNIE OCZYSZCZA WŁOSY I SKÓRĘ GŁOWY",
+    "NAWILŻA I CHRONI WŁOSY",
+    "ODŻYWIA",
+    "POMAGA ZACHOWAĆ NATURALNĄ RÓWNOWAGĘ SKÓRY GŁOWY",
+    "UŁATWIA ROZCZESYWANIE",
+    "DODAJE BLASKU, MIĘKKOŚCI I LEKKOŚCI",
+  ],
+  bannerLink:
+    "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-nutrizione-kosmetyki-do-wlosow-suchych.jpg",
+  pictureSectionTitle: "PRZYKŁADOWY TYTUŁ SEKCJI ZE ZDJĘCIAMI",
+  pictureItems: [
+    {
+      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-disciplina.jpg",
+      title: "Kemon actyva disciplina",
+      description: "Kontrola włosów kręconych i nieposłusznych",
+    },
+    {
+      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-nuova-fibra.jpg",
+      title: "Kemon nuova fibra",
+      description: "Wzmocnienie włosów słabych i uszkodzonych",
+    },
+    {
+      url: "https://www.brillar-sklep.pl/userdata/public/assets/kemon/kemon-actyva-colore-brillante.jpg",
+      title: "Kemon colore brillante",
+      description: "Wzmocnienie, ochrona i blask koloru",
+    },
+  ],
+};

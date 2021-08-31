753b145d6687c8155d97262c9da2eaaf7cae2e7d
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2021-08-31 21:29:56 +0200

    Fixed bug with undefined local storage values

--- src/components/AppContext/AppContext.js
+++ src/components/AppContext/AppContext.js
@@ -37,38 +37,38 @@ export const AppContext = React.createContext(appCtxDefaultValue);
 export const AppProvider = ({ children }) => {
   const localStorageValues = JSON.parse(localStorage.getItem("descriptionValues"));
 
-  const [producer, setProducer] = useState(localStorageValues.producer || appCtxDefaultValue.producer);
+  const [producer, setProducer] = useState(localStorageValues?.producer || appCtxDefaultValue.producer);
 
-  const [topHeader, setTopHeader] = useState(localStorageValues.topHeader || appCtxDefaultValue.topHeader);
-  const [middleHeader, setMiddleHeader] = useState(localStorageValues.middleHeader || appCtxDefaultValue.middleHeader);
-  const [bottomHeader, setBottomHeader] = useState(localStorageValues.bottomHeader || appCtxDefaultValue.bottomHeader);
+  const [topHeader, setTopHeader] = useState(localStorageValues?.topHeader || appCtxDefaultValue.topHeader);
+  const [middleHeader, setMiddleHeader] = useState(localStorageValues?.middleHeader || appCtxDefaultValue.middleHeader);
+  const [bottomHeader, setBottomHeader] = useState(localStorageValues?.bottomHeader || appCtxDefaultValue.bottomHeader);
 
-  const [paragraph, setParagraph] = useState(localStorageValues.paragraph || appCtxDefaultValue.paragraph);
+  const [paragraph, setParagraph] = useState(localStorageValues?.paragraph || appCtxDefaultValue.paragraph);
 
   const [enabledListSection, setEnabledListSection] = useState(
-    localStorageValues.enabledListSection || appCtxDefaultValue.enabledListSection
+    localStorageValues?.enabledListSection || appCtxDefaultValue.enabledListSection
   );
-  const [listSection, setListSection] = useState(localStorageValues.listSection || appCtxDefaultValue.listSection);
+  const [listSection, setListSection] = useState(localStorageValues?.listSection || appCtxDefaultValue.listSection);
 
   const [enabledBannerSection, setEnabledBannerSection] = useState(
-    localStorageValues.enabledBannerSection || appCtxDefaultValue.enabledBannerSection
+    localStorageValues?.enabledBannerSection || appCtxDefaultValue.enabledBannerSection
   );
   const [bannerSection, setBannerSection] = useState(
-    localStorageValues.bannerSection || appCtxDefaultValue.bannerSection
+    localStorageValues?.bannerSection || appCtxDefaultValue.bannerSection
   );
 
   const [enabledPicturesSection, setEnabledPicturesSection] = useState(
-    localStorageValues.enabledPicturesSection || appCtxDefaultValue.enabledPicturesSection
+    localStorageValues?.enabledPicturesSection || appCtxDefaultValue.enabledPicturesSection
   );
   const [pictureSectionTitle, setPictureSectionTitle] = useState(
-    localStorageValues.pictureSectionTitle || appCtxDefaultValue.pictureSectionTitle
+    localStorageValues?.pictureSectionTitle || appCtxDefaultValue.pictureSectionTitle
   );
-  const [pictureItems, setPictureItems] = useState(localStorageValues.pictureItems || appCtxDefaultValue.pictureItems);
+  const [pictureItems, setPictureItems] = useState(localStorageValues?.pictureItems || appCtxDefaultValue.pictureItems);
 
   const [enabledVideoSection, setEnabledVideoSection] = useState(
-    localStorageValues.enabledVideoSection || appCtxDefaultValue.enabledVideoSection
+    localStorageValues?.enabledVideoSection || appCtxDefaultValue.enabledVideoSection
   );
-  const [videoSection, setVideoSection] = useState(localStorageValues.videoSection || appCtxDefaultValue.videoSection);
+  const [videoSection, setVideoSection] = useState(localStorageValues?.videoSection || appCtxDefaultValue.videoSection);
 
   useEffect(() => {
     const descriptionValues = {

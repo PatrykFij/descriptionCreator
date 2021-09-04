3c3457f65274d54796ac10e05b411bc3ea757137
PatrykFij <patryk.fijalkowski1@gmail.com>
2021-09-04 22:46:08 +0200

    draft of source code pasting

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -58,8 +58,8 @@ export const Preview = () => {
       <div class="description-container">
         <div class="top-bar"></div>
         {producer && (
-          <div class="producer-logo">
-            <a href={`https://www.brillar-sklep.pl/` + producer}>
+          <div id="producer-logo-section" class="producer-logo">
+            <a data-id="a_1" href={`https://www.brillar-sklep.pl/` + producer}>
               <img
                 src={"https://www.brillar-sklep.pl/userdata/public/assets/producer-" + producer + ".png"}
                 alt="Logo producenta"
@@ -69,25 +69,32 @@ export const Preview = () => {
           </div>
         )}
         {(topHeader || middleHeader || bottomHeader) && (
-          <div class="section headers-section">
-            {topHeader && <h2>{topHeader}</h2>}
-            {middleHeader && <h3>{middleHeader}</h3>}
-            {bottomHeader && <h4>{bottomHeader}</h4>}
+          <div id="headers-section" class="section headers-section">
+            {topHeader && <h2 data-id="h2_1">{topHeader}</h2>}
+            {middleHeader && <h3 data-id="h3_1">{middleHeader}</h3>}
+            {bottomHeader && <h4 data-id="h4_1">{bottomHeader}</h4>}
           </div>
         )}
-        <div class="section description-section">
-          {paragraph && <p dangerouslySetInnerHTML={{ __html: paragraph }}></p>}
+        <div id="description_section" class="section description-section">
+          {paragraph && <p data-id="p_1" dangerouslySetInnerHTML={{ __html: paragraph }}></p>}
         </div>
         {enabledListSection && listSection.listItems.length > 0 && (
-          <div class="section list-section">
-            {listSection.title && <h4 class="header">{listSection.title}</h4>}
-            <ul class="list">{listSection.listItems.map((el) => (el ? <li>{el}</li> : null))}</ul>
+          <div id="list_section" class="section list-section">
+            {listSection.title && (
+              <h4 data-id="h4_1" class="header">
+                {listSection.title}
+              </h4>
+            )}
+            <ul data-id="ul_1" class="list">
+              {listSection.listItems.map((el) => (el ? <li>{el}</li> : null))}
+            </ul>
           </div>
         )}
         {enabledBannerSection && (
-          <div class="section banner-section">
+          <div id="banner-section" class="section banner-section">
             {bannerSection.imgFileName && (
               <img
+                data-id="img_1"
                 class="img-frame"
                 src={"https://www.brillar-sklep.pl/userdata/public/assets/" + bannerSection.imgFileName}
                 alt={bannerSection.imgAltTag}
@@ -97,42 +104,54 @@ export const Preview = () => {
           </div>
         )}
         {enabledPicturesSection && (
-          <div class="section">
-            {pictureSectionTitle && <h4 class="header">{pictureSectionTitle}</h4>}
-            <div class="image-section">
+          <div id="pictures-section" class="section">
+            {pictureSectionTitle && (
+              <h4 data-id="h4_1" class="header">
+                {pictureSectionTitle}
+              </h4>
+            )}
+            <div id="pictures-wrapper" class="image-section">
               {pictureItems.map((el, index) => (
                 <div key={index} class="image-container">
                   <img
+                    data-id="img_1"
                     src={"https://www.brillar-sklep.pl/userdata/public/assets/" + el.url}
                     alt={el.alt}
                     width="auto"
                   />
-                  {el.title && <h5>{el.title}</h5>}
-                  {el.description && <p>{el.description}</p>}
+                  {el.title && <h5 data-id="h5_1">{el.title}</h5>}
+                  {el.description && <p data-id="p_1">{el.description}</p>}
                 </div>
               ))}
             </div>
           </div>
         )}
         {enabledVideoSection && (
-          <>
-            <div class="section video-section">
-              {videoSection.sectionTitle && <h4 class="header">{videoSection.sectionTitle}</h4>}
-              {videoSection.description && (
-                <p class="section-description" dangerouslySetInnerHTML={{ __html: videoSection.description }}></p>
-              )}
-              <div class="video-wrapper">
-                <iframe
-                  width="100%"
-                  src={videoSection.videoUrl}
-                  title={videoSection.videoTitle}
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
+          <div id="video-section" class="section video-section">
+            {videoSection.sectionTitle && (
+              <h4 data-id="h4_1" class="header">
+                {videoSection.sectionTitle}
+              </h4>
+            )}
+            {videoSection.description && (
+              <p
+                data-id="p_1"
+                class="section-description"
+                dangerouslySetInnerHTML={{ __html: videoSection.description }}
+              ></p>
+            )}
+            <div class="video-wrapper">
+              <iframe
+                data-id="iframe_1"
+                width="100%"
+                src={videoSection.videoUrl}
+                title={videoSection.videoTitle}
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
-          </>
+          </div>
         )}
       </div>
     </PreviewWrapper>
--- src/components/SourceCodeDialog/SourceCodeDialog.js
+++ src/components/SourceCodeDialog/SourceCodeDialog.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../AppContext/AppContext";
 import {
   Button,
   Dialog,
@@ -11,6 +12,7 @@ import {
 
 export const SourceCodeDialog = ({ isOpen, setIsOpen }) => {
   const [sourceCode, setSourceCode] = useState();
+  const { setProducer } = useContext(AppContext);
 
   const handleClose = () => {
     setSourceCode();
@@ -18,7 +20,44 @@ export const SourceCodeDialog = ({ isOpen, setIsOpen }) => {
   };
 
   const handleApprove = () => {
-    setSourceCode();
+    var parser = new DOMParser();
+    var existingOffer = parser.parseFromString(sourceCode, "text/html").querySelector(".description-container");
+
+    if (existingOffer) {
+      const producer = existingOffer
+        .querySelector('#producer-logo-section [data-id="a_1"]')
+        .href.replace("https://www.brillar-sklep.pl/", "");
+
+      const topHeader = existingOffer.querySelector('#headers-section [data-id="h2_1"]').innerHTML;
+      const middleHeader = existingOffer.querySelector('#headers-section [data-id="h3_1"]').innerHTML;
+      const bottomHeader = existingOffer.querySelector('#headers-section [data-id="h4_1"]').innerHTML;
+
+      const descriptionSection = existingOffer.querySelector('#description_section [data-id="p_1"]').innerHTML;
+
+      const listSection = {
+        title: existingOffer.querySelector('#list_section [data-id="h4_1"]').innerHTML,
+        listItems: [...existingOffer.querySelector('#list_section [data-id="ul_1"]').childNodes].map(
+          (el) => el.innerHTML
+        ),
+      };
+
+      const bannerSection = {
+        imgFileName: existingOffer
+          .querySelector('#banner-section [data-id="img_1"]')
+          .src.replace("https://www.brillar-sklep.pl/userdata/public/assets/", ""),
+        imgAltTag: existingOffer.querySelector('#banner-section [data-id="img_1"]').alt,
+      };
+
+      const pictureSectionTitle = existingOffer.querySelector('#pictures-section [data-id="h4_1"]').innerHTML;
+      const pictureSectionItems = existingOffer.querySelectorAll(
+        "#pictures-section #pictures-wrapper .image-container"
+      );
+
+      console.log(pictureSectionItems);
+      // setProducer(producer);
+    } else {
+      console.log("Błędny format oferty");
+    }
     setIsOpen(false);
   };
 

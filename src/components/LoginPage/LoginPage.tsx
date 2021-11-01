298178d659520c7ee845dcb7ed7d4fe6681bc409
PatrykFij <patryk.fijalkowski1@gmail.com>
2021-11-01 15:52:15 +0100

    Added api request

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { Button } from "@material-ui/core";
+import axios from "axios";
+import { useToggle } from "hooks/useToggle";
+import Dialog from "components/Dialog";
 import TextInput from "components/Inputs/TextInput";
 import * as T from "./types";
-import Dialog from "components/Dialog";
-import { Button } from "@material-ui/core";
-import { useToggle } from "hooks/useToggle";
 
 const LoginPage = () => {
   const form = useForm<T.LoginFields>({
@@ -19,8 +20,20 @@ const LoginPage = () => {
   } = form;
 
   const onSubmit = handleSubmit(async () => {
-    const details = getValues();
-    console.log(details);
+    const { username, password } = getValues();
+    //brillar-sklep.pl/webapi/rest/auth
+    try {
+      const { data } = await axios.post("https://brillar-sklep.pl/webapi/rest/auth", {
+        auth: {
+          username: username,
+          password: password,
+        },
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   });
 
   const [bondholderMeetingsFormOpen, openBondholderMeetingsForm, closeBondholderMeetingsForm] = useToggle();
@@ -36,7 +49,7 @@ const LoginPage = () => {
         </>
       }
     >
-      <TextInput name="login" label="Login" control={control} errors={errors} required />
+      <TextInput name="username" label="Login" control={control} errors={errors} required />
       <TextInput name="password" label="Password" control={control} errors={errors} required />
     </Dialog>
   );
--- src/components/LoginPage/types.ts
+++ src/components/LoginPage/types.ts
@@ -1,4 +1,4 @@
 export interface LoginFields {
-  login: string;
+  username: string;
   password: string;
 }

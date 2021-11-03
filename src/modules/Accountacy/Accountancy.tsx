6b0f2448c5aadc61ae51fb741bb313aa985bda06
PatrykFij <patryk.fijalkowski1@gmail.com>
2021-11-03 20:35:33 +0100

    Added accountancy module

---
+++ src/modules/Accountacy/Accountancy.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-ui/core";
+
+const Accountancy = () => {
+  return (
+    <>
+      <h1>Księgowość</h1>
+      <Button>Pobierz dane</Button>
+    </>
+  );
+};
+
+export default Accountancy;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,7 @@ import LoginPage from "components/LoginPage";
 import DescriptionCreator from "modules/DescriptionCreator/DescriptionCreator";
 import * as URL from "./url";
 import PrivateRoute from "./PrivateRoute";
+import Accountacy from "modules/Accountacy";
 
 export default (
   <Layout>
@@ -12,6 +13,7 @@ export default (
         <LoginPage />
       </Route>
       <PrivateRoute path={URL.DESCRIPTION_CREATOR} component={DescriptionCreator} />
+      <PrivateRoute path={URL.ACCOUNTANCY} component={Accountacy} />
     </Switch>
   </Layout>
 );

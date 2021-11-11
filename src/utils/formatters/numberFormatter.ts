c2906bbebde6af01d32d23c5b56287f8eea0ab96
PatrykFij <patryk.fijalkowski1@gmail.com>
2021-11-11 18:23:57 +0100

    Added thousands separator in numbers

--- src/utils/formatters/numberFormatter.ts
+++ src/utils/formatters/numberFormatter.ts
@@ -1,7 +1,9 @@
 export const numberFormatter = (amount: string | number): string => {
   if (typeof amount === 'number') {
-    return amount.toFixed(2);
+    return amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
   } else {
-    return Number.parseFloat(amount).toFixed(2);
+    return Number.parseFloat(amount)
+      .toFixed(2)
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
   }
 };

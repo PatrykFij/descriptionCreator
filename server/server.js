3a85ac987b0b3799c3680450a1f606e35f19f2f7
PatrykFij <patryk.fijalkowski1@gmail.com>
2022-01-24 23:15:25 +0100

    Added serverr and proxy setup

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require('dotenv-flow').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const setupProxy = require('../src/setupProxy.js');
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || '8080';
+const contextPath =
+  process.env.CONTEXT_PATH === '/' ? '' : process.env.CONTEXT_PATH || '';
+
+setupProxy(app);
+
+app.use(`${contextPath}/`, express.static('build'));
+app.use('/*', (req, res) => {
+  res.header('X-Frame-Options', 'allow-from *');
+  return res.sendFile(path.resolve('build/index.html'));
+});
+
+const isLocalhost = Boolean(
+  host === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    host === '[::1]' ||
+    host === '0.0.0.0' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    host.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
+);
+
+const server = app.listen(port, host, () => {
+  console.log(
+    `Server started on http://${
+      isLocalhost ? 'localhost' : host
+    }:${port}${contextPath}`,
+    server.address(),
+  );
+});
---
+++ src/setupProxy.js
@@ -0,0 +1,32 @@
+const compression = require('compression');
+const proxy = require('http-proxy-middleware');
+const morgan = require('morgan');
+require('dotenv-flow').config();
+
+const backendUrl = process.env.SERVER_API || 'http://localhost:3001';
+const contextPath =
+  process.env.CONTEXT_PATH === '/' ? '' : process.env.CONTEXT_PATH || '';
+
+module.exports = function (app) {
+  app.set('x-powered-by', false);
+  app.use(compression());
+  app.use(
+    morgan(
+      '[:remote-addr] [:date[iso]] ":method :url HTTP/:http-version" status: :status time: :response-time ms contentLength: :res[content-length] ref: ":referrer" userAgent: ":user-agent"',
+    ),
+  );
+
+  app.use(`${contextPath}/env-config.js`, (req, res) => {
+    res.set('Content-Type', 'application/json');
+  });
+
+  app.use(
+    '/api',
+    proxy({
+      target: backendUrl,
+      changeOrigin: true,
+      logLevel: 'debug',
+      secure: false,
+    }),
+  );
+};
